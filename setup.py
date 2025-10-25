ae38de3d8a33b4d888a3a6a4aff1b13de53ec737
Syama Talari <syama5824@gmail.com>
2025-10-25 09:52:55 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "model_deployement_mlops"
+AUTHOR_USER_NAME = "syama"
+SRC_REPO = "mlproj"
+AUTHOR_EMAIL = "syama5824@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A Machine Learning Project for Deployment using MLOps",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
